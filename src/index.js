97ec9a532875f3ca8ccabb5459170ce1f891a2b7
AndersonKouadioDev <97792295+AndersonKouadioDev@users.noreply.github.com>
2022-05-16 11:04:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SSRProvider>
-      <Router basename="/build">
+      <Router basename="/public">
         <App />
       </Router>
     </SSRProvider>
