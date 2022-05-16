77b5621e619658c2cffa0ac305697789ef7c5f6b
AndersonKouadioDev <andersonkouadio0118@gmail.comm>
2022-05-16 10:49:48 +0000

    mi

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SSRProvider>
-      <Router>
+      <Router basename=".">
         <App />
       </Router>
     </SSRProvider>
